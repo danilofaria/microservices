90f2da84a62ad65bde575f8e5949feee98fd3f49
Danilo Faria <danilofaria@outlook.com>
2015-10-04 15:19:17 -0400

    Create routes

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/students', function (req, res) {
+  res.send('all students');
+});
+
+app.get('/students/:id', function (req, res) {
+  res.send('students' + req.params.id);
+});
+
+app.post('/students', function (req, res) {
+  res.send('created student');
+});
+
+app.put('/students/:id', function (req, res) {
+  res.send('update student ' + req.params.id);
+});
+
+app.delete('/students/:id', function (req, res) {
+  res.send('delete student' + req.params.id);
 });
 
 var server = app.listen(3000, function () {
