cbbe7f89f47fc304eb55f8d79152df0c6474c2bc
Danilo Faria <danilofaria@outlook.com>
2015-10-04 22:44:23 -0400

    Succesfully dockerized

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+console.log('Hi!');
+// Constants
+var DEFAULT_PORT = 8080;
+var PORT = process.env.PORT || DEFAULT_PORT;
+var MONGO_DEFAULT_PORT = 27017;
+var MONGO_PORT = process.env.MONGO_PORT_27017_TCP_PORT || MONGO_DEFAULT_PORT;
+var MONGO_DEFAULT_IP = '192.168.59.103';
+var MONGO_IP = process.env.MONGO_PORT_27017_TCP_ADDR || MONGO_DEFAULT_IP;
+
 var express = require('express');
 var bodyParser = require('body-parser');
 // create application/json parser
@@ -8,14 +17,20 @@ var app = express();
 app.use(require('body-parser').urlencoded({ extended: true }));
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.59.103:27017/test');
+var mongo_address = 'mongodb://' + MONGO_IP + ':' + MONGO_PORT + '/test';
+mongoose.connect(mongo_address);
 
 var Student = require('./models/student.js');
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:' + mongo_address));
 db.once('open', function (callback) {
   console.log('succesfully connected to mongodb');
+});
+
+app.get('/test', function (req,res) {
+ 	console.log('Hello');
+ 	res.send('Hello');
 });
 
 app.get('/students', function (req, res) {
@@ -90,7 +105,7 @@ app.delete('/students/:uni', function (req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
 
