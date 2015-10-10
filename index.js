a88068774f803d81a31d6d913f57a23279812432
Danilo Faria <danilofaria@outlook.com>
2015-10-10 17:23:19 -0400

    Router works well

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var httpProxy = require('http-proxy');
 var request = require('request');
 var _ = require('lodash');
+var Promise = require("bluebird");
 
 var DEFAULT_PORT = 8000;
 var PORT = process.env.PORT || DEFAULT_PORT;
 
 var proxy = httpProxy.createProxy();
 
-var students = ['http://192.168.59.103:8081', 'http://192.168.59.103:8082', 'http://192.168.59.103:8083'];
+var students = ['http://192.168.59.103:8081/students', 'http://192.168.59.103:8082/students'
+    //, 'http://192.168.59.103:8083'
+];
 
 var routes = {
     'http://192.168.59.103:8081': /students\/[a-hA-H]/i,
@@ -20,6 +23,49 @@ var findRoutingUrl = function (request_url) {
     return _.findKey(routes, function (regEx) {
         return regEx.test(request_url);
     });
+};
+
+var getAllStudents = function (url) {
+    var resolver = Promise.pending();
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            resolver.resolve(body);
+        } else {
+            resolver.reject(error);
+        }
+    });
+    return resolver.promise;
+};
+
+var postStudent = function (url, student) {
+    var resolver = Promise.pending();
+    request.post({
+        headers: {'content-type': 'application/json'},
+        url: url,
+        body: JSON.stringify(student)
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            resolver.resolve(body);
+        } else {
+            resolver.reject(error);
+        }
+    });
+    return resolver.promise;
+};
+
+var getJson = function (req) {
+    var resolver = Promise.pending();
+    var data = '';
+    req.on('data', function (chunk) {
+        data += chunk;
+    }).on('end', function () {
+        var json = JSON.parse(data);
+        resolver.resolve(json);
+    });
+    return resolver.promise;
 };
 
 require('http').createServer(function (req, res) {
@@ -35,37 +81,37 @@ require('http').createServer(function (req, res) {
     else {
         if (request_url == '/students/' || request_url == '/students') {
             if (req.method == 'POST') {
-                // Not working
-                var data = '';
-                req.on('data', function (chunk) {
-                    data += chunk;
-                }).on('end', function () {
-                    var json = JSON.parse(data),
-                        uni = json.uni;
+                getJson(req).then(function (student) {
+                    var uni = student.uni;
                     request_url = 'students/' + uni;
                     routing_url = findRoutingUrl(request_url);
-                    return proxy.web(req, res, {
-                        target: routing_url
-                    });
+                    return postStudent(routing_url + '/students', student);
+                }).then(function (body) {
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    return res.end(JSON.stringify(body));
+                }).catch(function (error) {
+                    res.writeHead(400, {'Content-Type': 'text/plain'});
+                    return res.end('Problem posting');
                 });
-                //return;
             } else if (req.method == 'GET') {
-                // has to merge with other urls
-                request({
-                    url: students[0] + '/students',
-                    json: true
-                }, function (error, response, body) {
-                    if (!error && response.statusCode === 200) {
+                var promises = _.map(students, getAllStudents);
+                Promise.all(promises)
+                    .then(function (allStudentArrays) {
+                        var allStudents = _.reduce(allStudentArrays, function (t, n) {
+                            return t.concat(n);
+                        });
                         res.writeHead(200, {'Content-Type': 'application/json'});
-                        res.end(JSON.stringify(body));
-                    } else {
-                        res.writeHead(200, {'Content-Type': 'text/plain'});
-                        return res.end('students');
-                    }
-                });
+                        return res.end(JSON.stringify(allStudents));
+                    })
+                    .catch(function (error) {
+                        res.writeHead(400, {'Content-Type': 'text/plain'});
+                        return res.end('Problem fetching all students');
+                    });
             }
         }
     }
-    //res.writeHead(400, {'Content-Type': 'text/plain'});
-    //return res.end('This url is not available.');
 }).listen(PORT);
+
+
+
+
