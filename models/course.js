733e5dd893f4317c5fead0854568c56e0a52a3ad
Danilo Faria <danilofaria@outlook.com>
2015-10-10 21:37:19 -0400

    student relationship added

--- models/course.js
+++ models/course.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 
 var courseSchema = mongoose.Schema({
     code: String,
-    title: String
+    title: String,
+    students: [{ uni: String }]
 });
+courseSchema.index({ code: 1}, { unique: true });
 var Course = mongoose.model('Course', courseSchema);
 
 module.exports = Course;
