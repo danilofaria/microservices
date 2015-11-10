7bcd063f44b76c246858712756b178a8f7e9bb61
Danilo Faria <danilofaria@outlook.com>
2015-11-10 00:04:57 -0500

    Improve router regex

--- router/config/config.js
+++ router/config/config.js
@@ -3,9 +3,9 @@ var students = ['http://192.168.59.103:8081',
     'http://192.168.59.103:8083'];
 
 var routes = {
-    'http://192.168.59.103:8081': /^\/students\/[a-hA-H]/i,
-    'http://192.168.59.103:8082': /^\/students\/[i-qI-Q]/i,
-    'http://192.168.59.103:8083': /^\/students\/[r-zR-Z]/i,
+    'http://192.168.59.103:8081': /^\/students\/[a-hA-H][a-zA-Z0-9]*$/i,
+    'http://192.168.59.103:8082': /^\/students\/[i-qI-Q][a-zA-Z0-9]*$/i,
+    'http://192.168.59.103:8083': /^\/students\/[r-zR-Z][a-zA-Z0-9]*$/i,
     'http://192.168.59.103:8090': /^\/courses/i
 };
 
