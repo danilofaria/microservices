06455b9815b1aaa79859ac9bd0031dcb348d7d11
Danilo Faria <danilofaria@outlook.com>
2015-11-15 15:39:16 -0500

    polling sqs messages

--- finance/index.js
+++ finance/index.js
@@ -1,5 +1,9 @@
+// some code from: http://www.bennadel.com/blog/2792-shedding-the-monolithic-application-with-aws-simple-queue-service-sqs-and-node-js.htm
+console.log(process.env);
+
 var Promise = require("bluebird"),
-    AWS = require('aws-sdk');
+    AWS = require('aws-sdk'),
+    chalk = require( "chalk" );
 
 var queueUrl = process.env.SOURCE_QUEUE_URL || 'https://sqs.us-east-1.amazonaws.com/575910043716/test',
 	microserviceUrl = process.env._URL || '192.168.59.103',
@@ -20,8 +24,22 @@ var sqs = new AWS.SQS({
 
 sqs = Promise.promisifyAll(sqs);
 
-// sqsA.sendMessageAsync({
-//     MessageBody: "data data"
+// sqs.sendMessageAsync({
+//     MessageBody: "{'replyTo': 'popo', 'payload': 'pipi', 'route': 'bunda', 'method': 'GET'}",
+//     MessageAttributes: {
+//     	replyTo: {
+//     		DataType: 'String',
+//     		StringValue: 'popo'
+//     	},
+//     	route: {
+//     		DataType: 'String',
+//     		StringValue: 'pipi'
+//     	},
+//     	method: {
+//     		DataType: 'String',
+//     		StringValue: 'pipi'
+//     	}
+//     }
 // })
 // .then(function(data){
 // 	console.log('all good');
@@ -32,23 +50,77 @@ sqs = Promise.promisifyAll(sqs);
 // 	console.log(err);
 // });
 
-sqs.receiveMessageAsync({
-        WaitTimeSeconds: 3, // Enable long-polling (3-seconds).
-        VisibilityTimeout: 10
-    })
-.then(function(data){
-	console.log('all good');
-	console.log(data);
-	return(
-        sqs.deleteMessageAsync({
-            ReceiptHandle: data.Messages[ 0 ].ReceiptHandle
-        })
-    );
-})
-.then(function( data ) {
-            console.log( "Message Deleted!" );
-        })
-.catch(function(err){
-	console.log('no');
-	console.log(err);
-});
+
+(function pollQueueForMessages() {
+
+	console.log( chalk.yellow( "Starting long-poll operation." ) );
+
+	sqs.receiveMessageAsync({
+		WaitTimeSeconds: 3, // Enable long-polling (3-seconds).
+		VisibilityTimeout: 10,
+		MessageAttributeNames: ['All']
+	})
+	.then(
+		function handleMessageResolve( data ) {
+			if ( ! data.Messages ) {
+				throw(
+					workflowError(
+						"EmptyQueue",
+						new Error( "There are no messages to process." )
+					)
+				);
+			}
+
+
+			console.log(data);
+			// ---
+			// TODO: Actually process the message in some way :P
+			// ---
+
+			console.log( chalk.green( "Deleting:", data.Messages[ 0 ].MessageId ) );
+			console.log( chalk.green( "MessageAttributes:", JSON.stringify(data.Messages[ 0 ].MessageAttributes )) );
+
+			// return(
+			// 	sqs.deleteMessageAsync({
+			// 		ReceiptHandle: data.Messages[ 0 ].ReceiptHandle
+			// 	})
+			// );
+		return 'popo';
+
+		}
+	)
+	.then(
+		function handleDeleteResolve( data ) {
+
+			console.log( chalk.green( "Message Deleted!" ) );
+
+		}
+	)
+
+	// Catch any error (or rejection) that took place during processing.
+	.catch(
+		function handleError( error ) {
+
+			switch ( error.type ) {
+				case "EmptyQueue":
+					console.log( chalk.cyan( "Expected Error:", error.message ) );
+				break;
+				default:
+					console.log( chalk.red( "Unexpected Error:", error.message ) );
+				break;
+			}
+
+		}
+	)
+
+	.finally( pollQueueForMessages );
+
+})();
+
+function workflowError( type, error ) {
+
+	error.type = type;
+
+	return( error );
+
+}
