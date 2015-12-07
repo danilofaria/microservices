f957d1eb4ebe0c458eedd3c76a9cbeeed0a55b15
Danilo Faria <danilofaria@outlook.com>
2015-12-06 22:07:56 -0500

    Add balance field to student

--- finance/models/student.js
+++ finance/models/student.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var studentSchema = mongoose.Schema({
     name: String,
     lastName: String,
+    balance: { type: Number, default: 0 },
     socialSecurityNumber: {type: String, required: true},
     uni: {type: String, required: true},
     tenantId: {type: String, required: true}
